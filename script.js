7868a0f9d30734e947563c630349f218ac242b2c
kanm4s <kanm4s@gmail.com>
2022-01-11 17:36:34 +0700

    Add random function to script

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 let maps = ['Fracture','Breeze','Icebox','Bind','Haven','Split','Ascent'];
 let players = ['Kan','Oh','Jo','Jame','Bomb','Peat','Sin','Tor','Kor'];
 
-let team1 = [];
+const randomTeam = (player) => {
+    let team1 = [];
+    let teamNumber1 =  Math.ceil(player.length / 2);
 
-let teamNumber1 =  Math.ceil(players.length / 2);
-let teamNumber2 =  players.length - teamNumber1;
-
-
-for (let i=0;i<teamNumber1;i++) {
-    let player = players[Math.floor(Math.random() * players.length)]
-    team1.push(player);
-    players.splice(players.findIndex(Element => Element === player),1)
-    
+    for (let i=0;i<teamNumber1;i++) {
+        let playertmp = player[Math.floor(Math.random() * player.length)]
+        team1.push(playertmp);
+        player.splice(player.findIndex(Element => Element === player),1)   
+    }
+    let team2 = player
+    console.log(team1)
+    console.log(team2)
+    return [team1,team2]
 }
+let [team1,team2] = randomTeam(players)
 
-let team2 = players
 const mapChoose = maps[Math.floor(Math.random() * maps.length)]
 
 console.log(`Map is ${mapChoose}`)
 console.log(`team 1: ${team1}`)
-console.log(`team 1: ${team2}`)
+console.log(`team 2: ${team2}`)
